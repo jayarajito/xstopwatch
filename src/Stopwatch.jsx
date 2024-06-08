89ba65e7801eb945bc34c51e3c312f9ef0aa6fd7
JAYARAJ <jayarajcs2011@hotmail.com>
2024-06-08 18:15:40 +0530

    xstopwatch

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+
+function Stopwatch() {
+  const [timeinSec, setTimeinSec] = useState(0);
+  const [timerOn, setTimierOn] = useState(false);
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+  const startHandler = () => {
+    console.log("Start Handler");
+    //setTimierOn(!timerOn);
+    setTimierOn((preTimerState) => !preTimerState);
+    //setTimeinSec((prevState) => prevState + 1);
+  };
+  const resetHandler = () => {
+    console.log("Reset Handler");
+    setTimeinSec((prevState) => (prevState = 0));
+    setTimierOn(false);
+  };
+  useEffect(() => {
+    let myTimer;
+    console.log("Timer is ON or OFF", timerOn);
+    if (timerOn) {
+      myTimer = setInterval(() => {
+        setTimeinSec((prevState) => prevState + 1);
+      }, 1000);
+    } else {
+      clearInterval(myTimer);
+    }
+    return () => clearInterval(myTimer);
+  }, [timerOn]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+        padding: "10px",
+        border: "8px solid black",
+        borderRadius: "12px",
+      }}
+    >
+      <div>
+        <h1>Stopwatch</h1>
+        <h2>Time:{formatTime(timeinSec)}</h2>
+        <button onClick={startHandler}>{timerOn ? "Stop" : "Start"}</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
